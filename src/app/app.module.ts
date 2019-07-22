c8795dd14243f61f5bba35de3e0322f27b3b0c27
Jan Vrastil <jan.vrastil@bisimulations.com>
2019-07-22 15:11:48 +0200

    Add Google AdSense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdsenseModule } from 'ng2-adsense';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-7990836138695617',
+      pageLevelAds: true
+    }),
     BrowserModule
   ],
   providers: [],
